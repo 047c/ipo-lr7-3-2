14a1343656600e344769a01481d71a59fdeb2530
047c <maks.smirnov.smolensk@gmail.com>
2024-12-16 22:09:19 +0300

    renaming

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+
 import json  # Подключаем json
 
-linii = '-' * 22  # Добавляем элемент дизайна
+lines = '-' * 22  # Добавляем элемент дизайна
 with open("dump.json", 'r', encoding='utf-8') as file:  # Открываем файл, выдав имя файл
     dump = json.load(file)  # Загружаем в перменную дамп содержимое json
 while True:  # Пока истинно(программа выполняется, пока не будет завершена извне)
@@ -9,17 +10,17 @@ while True:  # Пока истинно(программа выполняется
     for item in dump:  # Для  каждого предмета
         if item['model'] == 'data.skill':  # Если имеет значение квалификации в поле модель
             if item["fields"]['code'] == find:  # Если код равен
-                for specialty in dump:
-                    if specialty['model'] == 'data.specialty':
-                        if specialty['pk'] == item['fields']['specialty']:
-                            print(f'{linii} Найдено {linii}')
-                            print(
-                                f'{specialty["fields"]["code"]} >> Специальность'
-                                f' {specialty["fields"]["title"]}, {specialty["fields"]["c_type"]}')
-                            print(f"{item['fields']['code']} >> Квалификация {item['fields']['title']}")
-                            print(linii + linii)
-                            status = True
-    if not status:
-        print(f'{linii} Не найдено {linii}')
-        print("Не получилось ничего найти по вашей квалификации")
-        print(linii + linii)
+                for specialty in dump: # Для каждой специальности в файле
+                    if specialty['model'] == 'data.specialty': # Если модель специальности равна data.specialty
+                        if specialty['pk'] == item['fields']['specialty']: # Если pk равен нужному
+                            print(f'{lines} Найдено {lines}') # Выводим
+                            print( # Найденную специальность
+                                f'{specialty["fields"]["code"]} >> Специальность' # С
+                                f' {specialty["fields"]["title"]}, {specialty["fields"]["c_type"]}') # Красивым
+                            print(f"{item['fields']['code']} >> Квалификация {item['fields']['title']}") # Оформлением
+                            print(lines + lines) # А также
+                            status = True # Устанавливаем статус
+    if not status: # Который воспрепятствует
+        print(f'{lines} Не найдено {lines}') # Выводу сообщения
+        print("Не получилось ничего найти по вашей квалификации") # Об отсутствии соответствия
+        print(lines + lines) # В файле
